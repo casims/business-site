ed4d3ce66644dad2d2a29c7b7dc3013a626294b9
Connor <csims9@hotmail.com>
2023-11-04 13:17:21 -0700

    Add styles for nav bar when scrolled down

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 'use strict';
 
 const navCont = {
+    observer: new IntersectionObserver(
+        ([e]) => e.target.classList.toggle("stickied", e.intersectionRatio < 1),
+        {threshold: [1]}
+    ),
     menuExpanded: false,
     navListeners: function() {
         document.getElementById('nav-button').addEventListener('click', function() {
             navCont.menuExpanded = !navCont.menuExpanded;
             navCont.toggleMenu();
         });
+        this.observer.observe(document.querySelector("header"));
     },
     toggleMenu: function() {
         const navTarget = document.getElementById('nav-container');
