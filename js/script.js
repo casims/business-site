cedeb3aaec24ff4d7f9629754767867ed9c3af90
Connor <csims9@hotmail.com>
2023-11-08 12:43:31 -0800

    Add further styles for main page

--- js/script.js
+++ js/script.js
@@ -24,8 +24,39 @@ const navCont = {
             navTarget.classList.remove('menu-open');
         };
     },
+    accordHeightCollapsed: '6.5rem',
+    accordExpand: function(accord) {
+        let accordHeight = accord.scrollHeight;
+        accord.style.height = accordHeight + 'px';
+        accord.setAttribute('expanded', 'true');
+    },
+    accordCollapse: function(accord) {
+        accord.style.height = this.accordHeightCollapsed;
+        accord.setAttribute('expanded', 'false');
+    },
+    accordListeners: function() {
+        // Functionality for accordians on individual project page, also expands first accordian on page load
+        let accordSections = document.getElementsByClassName('accord');
+        let accordButtons = document.getElementsByClassName('accord-button');
+        navCont.accordExpand(accordSections[0]);
+        for (let i = 0; i < accordButtons.length; i++) {
+            accordButtons[i].addEventListener('click', function(event) {
+                let accordTarget = event.target.parentElement;
+                let isAccordExpanded = accordTarget.getAttribute('expanded') === 'true';
+                if (!isAccordExpanded) {
+                    for (let accordSection of accordSections) {
+                        navCont.accordCollapse(accordSection);
+                    };
+                    navCont.accordExpand(accordTarget);
+                } else {
+                    navCont.accordCollapse(accordTarget);
+                };
+            });
+        };
+    },
 };
 
 window.onload = function() {
     navCont.navListeners();
+    navCont.accordListeners();
 };
